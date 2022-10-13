101325c8dcd04a6aab3dc2ccb4a164916388d0d0
Eldad Uzman <eldadu1985@gmail.com>
2022-10-13 09:19:57 +0300

    add statistical analysis notebook

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 
-NUMBER_OF_OBSERVATIONS = 2
+NUMBER_OF_OBSERVATIONS = 10
 
-HOW_MANY_THREADS = 100
-RAMP_UP = 1
+HOW_MANY_THREADS = 500
+RAMP_UP = 10
 DURATION = 600
 
 
-# python:
 for i in range(NUMBER_OF_OBSERVATIONS):
+    # python:
     os.system(
         f"docker run -v C:\\benchmark\\py_outs:/usr/src/app/output -e HOW_MANY_THREADS={HOW_MANY_THREADS} -e RAMP_UP={RAMP_UP} -e DURATION={DURATION} py-jmeter-benchmark"
     )
 
-# java:
-for i in range(NUMBER_OF_OBSERVATIONS):
+    # java:
     os.system(
-        "docker run -v C:\\benchmark\\java_outs:/usr/src/app/output -e HOW_MANY_THREADS={HOW_MANY_THREADS} -e RAMP_UP={RAMP_UP} -e DURATION={DURATION} java-jmeter-benchmark"
+        f"docker run -v C:\\benchmark\\java_outs:/usr/src/app/output -e HOW_MANY_THREADS={HOW_MANY_THREADS} -e RAMP_UP={RAMP_UP} -e DURATION={DURATION} java-jmeter-benchmark"
     )
+
